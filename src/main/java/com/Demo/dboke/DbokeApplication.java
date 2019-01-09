c176d40ab020eeab948c345e8257ea1e27c48811
dengshuqiang <461250239@qq.com>
2019-01-09 16:39:46 +0800

    20190109

---
+++ src/main/java/com/Demo/dboke/DbokeApplication.java
@@ -0,0 +1,14 @@
+package com.Demo.dboke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbokeApplication.class, args);
+	}
+
+}
+
